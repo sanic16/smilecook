7df535155087ea65667bfa71707d6c191b062cd8
julio <julio.sanic.gt.256@gmail.com>
2024-02-05 19:37:09 -0600

    category routes

--- resources/category.py
+++ resources/category.py
@@ -6,6 +6,21 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 from decorators import admin_required
 
 class CategoryListResource(Resource):
+    def get(self):
+        categories = Category.get_all_categories()
+        data = []
+        for category in categories:
+            data.append({
+                'id': category.id,
+                'name': category.name
+            })
+
+        return {
+            'categories': data
+        }, HTTPStatus.OK
+
+
+
     @jwt_required()
     @admin_required
     def post(self):
